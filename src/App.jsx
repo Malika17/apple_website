63f946522e37c58186f47a04d26b16ec7f79a181
Malika17 <malikasharma1711@gmail.com>
2024-04-03 18:11:50 +0530

    video carousel done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
+import VideoCarousel from "./components/VideoCarousel";
 
 const App = () => {
   return (
